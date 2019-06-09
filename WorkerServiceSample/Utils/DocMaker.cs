71feeaeb6c7827fafd90f397813c259732d59446
shahedc <shahedc@gmail.com>
2019-06-08 23:31:41 -0400

    Moved ebook generated into separate DocMaker class and updated Worker class. Also added sample output files.

---
+++ WorkerServiceSample/Utils/DocMaker.cs
@@ -0,0 +1,55 @@
+﻿using HtmlAgilityPack;
+using MariGold.OpenXHTML;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace WorkerServiceSample.Utils
+{
+    public class DocMaker
+    {
+        public static void MakeDoc(string pageUrl)
+        {
+            // Get HTML from website
+            string htmlContent = string.Empty;
+            string outputFileName = "_output.docx";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(pageUrl);
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                htmlContent = reader.ReadToEnd();
+
+            }
+            outputFileName = request.RequestUri.AbsolutePath.Substring(1);
+
+            //Console.WriteLine(html);
+
+
+            // Get content
+            HtmlDocument htmlDoc = new HtmlDocument();
+            //htmlDoc.Load(@"file.htm");
+            htmlDoc.LoadHtml(htmlContent);
+
+            HtmlNode node = htmlDoc.DocumentNode.SelectSingleNode("//*[@id=\"content\"]");
+            string nodeContent = (node == null) ? "Error, id not found" : node.InnerHtml;
+
+
+            Console.WriteLine("Making your document...");
+
+            // Create DOCX file
+            WordDocument wordDoc = new WordDocument($"{outputFileName}.docx");
+            wordDoc.Process(new HtmlParser(nodeContent));
+            wordDoc.Save();
+
+            //WordDocument doc = new WordDocument("blog.docx");
+            //doc.BaseURL = "http:\\wakeupandcode.com";
+            //doc.Process(new HtmlParser("<a href=\"index.htm\">sample</a>"));
+            //doc.Save();
+        }
+    }
+}
--- WorkerServiceSample/Worker.cs
+++ WorkerServiceSample/Worker.cs
@@ -1,14 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
-using HtmlAgilityPack;
-using MariGold.OpenXHTML;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WorkerServiceSample.Utils;
 
 namespace WorkerServiceSample
 {
@@ -23,7 +21,6 @@ namespace WorkerServiceSample
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-
             //// TEST: infinite loop
             //while (!stoppingToken.IsCancellationRequested)
             //{
@@ -31,46 +28,15 @@ namespace WorkerServiceSample
             //    await Task.Delay(1000, stoppingToken);
             //}
 
+            string pageUrl1 = "https://wakeupandcode.com/unit-testing-in-asp-net-core";
+            string pageUrl2 = "https://wakeupandcode.com/validation-in-asp-net-core";
 
-            // Get HTML from website
-            string htmlContent = string.Empty;
-            string pageUrl = "https://wakeupandcode.com/key-vault-for-asp-net-core-web-apps";
-            string outputFileName = "_output.docx";
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(pageUrl);
-
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-            using (Stream stream = response.GetResponseStream())
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                htmlContent = reader.ReadToEnd();
-
-            }
-            outputFileName = request.RequestUri.AbsolutePath.Substring(1);
-
-            //Console.WriteLine(html);
+            _logger.LogInformation("Making doc 1 at: {time}", DateTimeOffset.Now);
+            DocMaker.MakeDoc(pageUrl1);
 
 
-            // Get content
-            HtmlDocument htmlDoc = new HtmlDocument();
-            //htmlDoc.Load(@"file.htm");
-            htmlDoc.LoadHtml(htmlContent);
-
-            HtmlNode node = htmlDoc.DocumentNode.SelectSingleNode("//*[@id=\"content\"]");
-            string nodeContent = (node == null) ? "Error, id not found" : node.InnerHtml;
-
-
-            Console.WriteLine("Making your ebook...");
-
-            // Create DOCX file
-            WordDocument wordDoc = new WordDocument($"{outputFileName}.docx");
-            wordDoc.Process(new HtmlParser(nodeContent));
-            wordDoc.Save();
-
-            //WordDocument doc = new WordDocument("blog.docx");
-            //doc.BaseURL = "http:\\wakeupandcode.com";
-            //doc.Process(new HtmlParser("<a href=\"index.htm\">sample</a>"));
-            //doc.Save();
+            _logger.LogInformation("Making doc 2 at: {time}", DateTimeOffset.Now);
+            DocMaker.MakeDoc(pageUrl2);
 
         }
     }
