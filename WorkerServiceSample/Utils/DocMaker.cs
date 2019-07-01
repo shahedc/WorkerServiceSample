f6bc80f18b3af5cabe584d46a028b0ce478da030
shahedc <shahedc@gmail.com>
2019-07-01 16:43:01 -0400

    WIP: experimental doc generation, with acceptable code formatting but image width still needs fixing

--- WorkerServiceSample/Utils/DocMaker.cs
+++ WorkerServiceSample/Utils/DocMaker.cs
@@ -35,15 +35,49 @@ namespace WorkerServiceSample.Utils
             //htmlDoc.Load(@"file.htm");
             htmlDoc.LoadHtml(htmlContent);
 
-            HtmlNode node = htmlDoc.DocumentNode.SelectSingleNode("//*[@id=\"content\"]");
-            string nodeContent = (node == null) ? "Error, id not found" : node.InnerHtml;
+            htmlDoc.DocumentNode.SelectNodes("//pre")
+                .Append(htmlDoc.CreateTextNode("first line\nsecond line"));
 
+            var preNodes = htmlDoc.DocumentNode.SelectNodes("//pre");
+            foreach (HtmlNode htmlPreNode in preNodes)
+            {
+                var replacedText1 = htmlPreNode.InnerText + "_translated";
+                var replacedText2 = htmlPreNode.InnerText.Replace('\n', 'X');
+                var replacedText3 = htmlPreNode.InnerText.Replace(System.Environment.NewLine, "Y");
+                var replacedText4 = htmlPreNode.InnerText.Replace(System.Environment.NewLine, "NEWLINE");
+
+                var TextWithFormatting = "<span style=\"color: #808080;font-family: Courier New;\">"
+                    + replacedText4 + "</span>";
+
+                htmlPreNode.ParentNode.ReplaceChild(
+                    HtmlTextNode.CreateNode(
+                        TextWithFormatting), htmlPreNode);
+            }
+            //
+
+            HtmlNode htmlNode = htmlDoc.DocumentNode.SelectSingleNode("//*[@id=\"content\"]");
+            string htmlNodeContent = (htmlNode == null) ? "Error, id not found" : htmlNode.InnerHtml;
+
+            //htmlNode.InnerHtml = htmlNodeContent;
+
+
+            //// Replace <pre> tag with custom font
+            //htmlNodeContent = htmlNodeContent.Replace(
+            //    "<pre>", "<span style=\"color: #ff0000;font-family: Courier New;\">");
+            //htmlNodeContent = htmlNodeContent.Replace("</pre>", "</span>");
+
+            // replace placeholders with actual <br> tags
+            htmlNodeContent = htmlNodeContent.Replace("NEWLINE", "<br />");
+
+
+            // Write html node's content to text file.
+            File.WriteAllText(@"HtmlContent.txt", htmlNodeContent);
 
             Console.WriteLine("Making your document...");
 
             // Create DOCX file
             WordDocument wordDoc = new WordDocument($"{outputFileName}.docx");
-            wordDoc.Process(new HtmlParser(nodeContent));
+            wordDoc.Process(new HtmlParser(htmlNodeContent));
             wordDoc.Save();
 
             //WordDocument doc = new WordDocument("blog.docx");
